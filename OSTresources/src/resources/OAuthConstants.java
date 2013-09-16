ba015afe7b5a750aae6450e7b702fedf2fccefc6
Nuno Rebelo <nrebelo@ipn.pt>
2013-09-16 10:07:16 +0100

    OAuth 2 demo: first commit.
    add all files

---
+++ OSTresources/src/resources/OAuthConstants.java
@@ -0,0 +1,28 @@
+package resources;
+
+
+/**
+ * OAuthConstants is where OAuth related constants
+ * are set
+ */
+
+public class OAuthConstants {
+
+
+	
+	public static final String CONSUMER_KEY 	= "YOUR_CONSUMER_KEY";
+	public static final String CONSUMER_SECRET 	= "YOUR_CONSUMER_SECRET";
+	
+	public static final String SCOPE 			= "user";
+	
+	public static final String REQUEST_URL 		= "https://auth.ost.pt/v2/request_token/";
+	public static final String ACCESS_URL 		= "https://auth.ost.pt/v2/access_token/";
+	public static final String AUTHORIZE_URL 	= "https://auth.ost.pt/v2/authorize/";
+	public static final String USER_URL 		= "https://api.ost.pt/users";
+			
+	public static final String ENCODING 		= "UTF-8";
+
+	public static final String	OAUTH_CALLBACK_SCHEME	= "x-oauthflow";
+	public static final String	OAUTH_CALLBACK_HOST		= "callback";
+	public static final String	OAUTH_CALLBACK_URL		= OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+}
